6b68a7312bc82d5b7bddadb36ffa1c8e1c36eac0
ChrisBlues <chris.blues@free.fr>
2025-10-06 23:18:12 +0200

    Les modales de Gallery sont opérationnelles. On complète  maintenant les 'Services'.

--- script.js
+++ script.js
@@ -35,25 +35,25 @@ window.addEventListener('scroll', () => {
   }
 });
 
-// Modale de Gallery qui fonctionne sur Elsewhere3eVersion
-    document.addEventListener("DOMContentLoaded", function () {
-      const popImages = document.querySelectorAll(".pop");
-      const modal = new bootstrap.Modal(document.getElementById("imagemodal"));
-      const modalImg = document.querySelector(".modal-image");
-      const closeBtn = document.querySelector(".close-modal");
+// Modale de Gallery
+document.addEventListener("DOMContentLoaded", function () {
+  const popImages = document.querySelectorAll(".pop");
+  const modal = new bootstrap.Modal(document.getElementById("imagemodal"));
+  const modalImg = document.querySelector(".modal-image");
+  const closeBtn = document.querySelector(".close-modal");
 
-      // Clic sur image agrandie pour fermer
-      modalImg.addEventListener("click", function () {
-        modal.hide();
-      });
+  // Clic sur image agrandie pour fermer
+  modalImg.addEventListener("click", function () {
+    modal.hide();
+  });
 
-      // Clic sur image miniature pour ouvrir
-      popImages.forEach(function (el) {
-        el.addEventListener("click", function (e) {
-          e.preventDefault();
-          const src = el.querySelector("img").getAttribute("src");
-          modalImg.setAttribute("src", src);
-          modal.show();
-        });
-      });
+  // Clic sur image miniature pour ouvrir
+  popImages.forEach(function (el) {
+    el.addEventListener("click", function (e) {
+      e.preventDefault();
+      const src = el.querySelector("img").getAttribute("src");
+      modalImg.setAttribute("src", src);
+      modal.show();
     });
+  });
+});
